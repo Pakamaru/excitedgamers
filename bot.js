77f0c83b11ee9de69da9377ebdaa2bef88cb6626
Pakamaru <sanamaru99@gmail.com>
2018-01-18 21:01:29 +0100

    Add files via upload
    1. The prefix has been changed from "#" to "-" due to the auto channel selecting of discord
    2. The bot will now send an auto message in the welcome channel when someone joins
    3. The rank/region/channels are now ready to be used to set your information. The bot will auto delete any message sent to keep the chat clean (reconsider this).

--- bot.js
+++ bot.js
@@ -2,14 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require("./auth.json");
 
-const welcome = "403132004725424128";
-const rank = "403136969787572224";
-const region = "403136986958921741";
-const roles = "403137045142437890";
+const welcomeID = "402813648164225027";
+const rankID = "403136969787572224";
+const regionID = "403136986958921741";
+const rolesID = "403137045142437890";
+
+const divisions = ["bronze","silver","gold","platinum","diamond"];
+const ranks = ["I","II","III","IV","V"];
+const regions = ["euw","eune","na","ru","lan","las","br","oce","tr","jp","kr","cn"];
+const roles  = ["adc","support","mid","jungle","top"];
 
 
 client.on("ready", () => {
   client.user.setGame("teemo");
+});
+
+client.on("guildMemberAdd", (member) => {
+  defaultChannel.send("Welcome "+member+"!\nHave fun and type '"+auth.prefix+"agree' after you read the rules in #info");
 });
 
 client.on("message", (message) => {
@@ -17,9 +26,9 @@ client.on("message", (message) => {
   if(message.author.bot) return ;
 
   const rawArgs = message.content.split(/ +/g); //returns an array
-  if(message.channel.id === rank) rank(message, rawArgs);
-  if(message.channel.id === region) region(message, rawArgs);
-  if(message.channel.id === roles) roles(message, rawArgs);
+  if(message.channel.id === rankID) setRank_(message, rawArgs);
+  if(message.channel.id === regionID) setRegion_(message, rawArgs);
+  if(message.channel.id === rolesID) setRoles_(message, rawArgs);
 
 
   if(message.content.indexOf(auth.prefix) !== 0) return ;
@@ -28,7 +37,7 @@ client.on("message", (message) => {
   const command = args.shift().toLowerCase(); //returns the first parameter of the input of the user
   const commandArgs = args.join(' '); //returns all the parameters of the input of the user separated by a space
 
-  if(message.channel.id === welcome && command === "accept") accept(message);
+  if(message.channel.id === welcomeID && command === "accept") accept_(message);
 
 
 
@@ -36,9 +45,82 @@ client.on("message", (message) => {
 });
 client.login(auth.token);
 
-function accept(m){
+function accept_(m){
   let member = m.guild.member(m.author);
   let role = m.guild.roles.find("name", "member");
   member.addRole(role);
-  return m.content.delete;
+  return m.delete();
+}
+function setRank_(m, a){
+  let member = m.guild.member(m.author);
+  if(m.content.indexOf(auth.prefix) === 0) removeRank_(member);
+  else{
+    try{
+      let role = [m.guild.roles.find("name", a[0].toLowerCase()), m.guild.roles.find("name", a[1].toUpperCase())];
+      member.addRoles(role);
+    }catch(err){
+      console.log(err);
+    }
+  }
+return m.delete();
+}
+function removeRank_(member){
+  for(var i=0; i<member.roles.array().length; i++){
+    for(var j=0; j<divisions.length; j++){
+      if(member.roles.array()[i].name === divisions[j]){
+        member.removeRole(member.roles.array()[i]);
+      }
+    }
+    for(var j=0; j<ranks.length; j++){
+      if(member.roles.array()[i].name === ranks[j]){
+        member.removeRole(member.roles.array()[i]);
+      }
+    }
+  }
+}
+function setRegion_(m, a){
+  let member = m.guild.member(m.author);
+  if(m.content.indexOf(auth.prefix) === 0) removeRegion_(member);
+  else{
+    try{
+      let role = m.guild.roles.find("name", a[0].toLowerCase());
+      member.addRole(role);
+    }catch(err){
+      console.log(err);
+    }
+  }
+  return m.delete();
+}
+function removeRegion_(member){
+  for(var i=0; i<member.roles.array().length; i++){
+    for(var j=0; j<regions.length; j++){
+      if(member.roles.array()[i].name === regions[j]){
+        member.removeRole(member.roles.array()[i]);
+      }
+    }
+  }
+}
+function setRoles_(m, a){
+  let member = m.guild.member(m.author);
+  if(m.content.indexOf(auth.prefix) === 0) RemoveRole_(member);
+  else{
+    for(var i=0; i<a.length; i++){
+      try{
+        let role = m.guild.roles.find("name", a[0].toLowerCase());
+      }catch(err){
+        console.log(err);
+      }
+      member.addRole(role)
+    }
+  }
+  return m.delete();
+}
+function RemoveRole_(member){
+  for(var i=0; i<member.roles.array().length; i++){
+    for(var j=0; j<roles.length; j++){
+      if(member.roles.array()[i].name === roles[j]){
+        member.removeRole(member.roles.array()[i]);
+      }
+    }
+  }
 }
